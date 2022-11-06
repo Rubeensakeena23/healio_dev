c1f63306bbdbda6cf5dfc6baa60693af5d544caa
Rubeensakeena23 <rubeensakeena23@gmail.com>
2022-11-06 14:41:11 +0530

    Initial commit

---
+++ RasaUI/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Basic from "./components/Basic"
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Healio</h1>
+      <Basic />
+    </div>
+  );
+}
+
+export default App;
